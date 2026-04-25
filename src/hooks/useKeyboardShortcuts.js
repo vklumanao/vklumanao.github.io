e11c9723e9b6d58e76112b111f57fc0293af4a29
Vicryl Kez Lumanao <wariyuri3@gmail.com>
2026-04-25 20:39:09 +0800

    feat: add src/hooks/useKeyboardShortcuts.js

---
+++ src/hooks/useKeyboardShortcuts.js
@@ -0,0 +1,8 @@
+﻿import { useEffect } from "react";
+
+export function useKeyboardShortcuts(handler) {
+  useEffect(() => {
+    window.addEventListener("keydown", handler);
+    return () => window.removeEventListener("keydown", handler);
+  }, [handler]);
+}
